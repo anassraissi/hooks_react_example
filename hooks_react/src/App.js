770e319f5445a52471976ea7c4321340ba10a7e8
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-11-12 15:49:47 +0100

    useState befor use custom hook

--- hooks_react/src/App.js
+++ hooks_react/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count,setCount]=useState(0);
+          const increment=()=>{
+              setCount(c=>c+1)
+          } 
+          const decrement=()=>{
+            setCount(c=> c>0 ? c-1 : c)
+          } 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
   );
 }
