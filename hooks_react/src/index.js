a103835a1a635de98a43a9d5068bc49d8efc5999
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-11-13 13:08:18 +0100

    manage thing

--- hooks_react/src/index.js
+++ hooks_react/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Custom from './Custom_hooks/App';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Custom></Custom>
   </React.StrictMode>
 );
 
