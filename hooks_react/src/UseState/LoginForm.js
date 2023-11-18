fcd408df019ab763e95a1120371b67e3cb3bd322
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-11-18 14:14:55 +0100

    zadna password wsafi

--- hooks_react/src/UseState/LoginForm.js
+++ hooks_react/src/UseState/LoginForm.js
@@ -3,13 +3,18 @@ import React, { useState } from 'react'
 
 export  function LoginFormA() {
     const [email,setEmail]=useState(''); // we dont type React.useState ==> cause we already declared above there.
+    const [password,setPassword]=useState(''); 
      function handelEmail(e){   // must type function 
         setEmail(e.target.value)
+    }
+     function handelPassword(e){   // must type function 
+        setPassword(e.target.value)
     }
   return (
     <div>
        <h1>function example</h1> 
-       <input value={email} onChange={handelEmail}/>
+       <input value={email} onChange={handelEmail} placeholder='Email ...'/> <br></br>
+       <input type='password' value={password} onChange={handelPassword} placeholder='Password ...'/>
 
 
     </div>
@@ -18,18 +23,25 @@ export  function LoginFormA() {
 
 export class LoginFormB extends React.Component {
     state={
-        email:''
+        email:'',
+        password:''
     }
-   handelEmail=(e)=>{
+   handelEmail=(e)=>{  // not type function or const cause we in class component
         this.setState({
                 email:e.target.value
+        })
+    };
+   handelPassword=(e)=>{  // not type function or const cause we in class component
+        this.setState({
+                password:e.target.value
         })
     };
   render() {
     return (
       <div>
         <h1>Class example</h1>
-        <input value={this.state.email} onChange={this.handelEmail}/>
+        <input value={this.state.email} onChange={this.handelEmail} placeholder='Email ...'/> <br></br>
+        <input type='password' value={this.state.password} onChange={this.handelPassword} placeholder='password ...'/>
       </div>
     )
   }
