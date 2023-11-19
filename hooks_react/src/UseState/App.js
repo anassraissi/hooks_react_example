6315902b09ca51d52e97aab369c32c66602c6358
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-11-19 14:28:05 +0100

    work on life cycle of components on easy way

--- hooks_react/src/UseState/App.js
+++ hooks_react/src/UseState/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {LoginFormA,LoginFormB  } from "./LoginForm";
 
 const App = () => {
+  const [visible,setVisible]=useState(true);
+    
   return (
     <div>
+      {visible ?<> 
         <LoginFormA></LoginFormA>
         <LoginFormB></LoginFormB>
+        </>  : null}
+        <button onClick={()=>{setVisible(!visible)}}>hide/show component</button>
     </div>
   )
 }
--- hooks_react/src/UseState/LoginForm.js
+++ hooks_react/src/UseState/LoginForm.js
@@ -1,9 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 export  function LoginFormA() {
     const [email,setEmail]=useState(''); // we dont type React.useState ==> cause we already declared above there.
     const [password,setPassword]=useState(''); 
+    //---------------------chargement dyal component ------------
+    useEffect(()=>{
+      console.log('FunctionComponents => chargemet dyal page');
+      return(()=>{
+        console.log('FunctionComponents => katlgha component dyal page'); // bhal if else il makanech component khdam bhadi.
+      })
+    },[])  // [] kangoliha mnin tabadal valeur li wast [] khadmi. fhal lhala ghatkhdam ghi mra wahda flawal. 
+
+  //----------------------------componentDidUpdate dyal email------------------------
+
+       
+      useEffect(()=>{
+       if (email ==""){   // mnin tcharga awal mara mykhdamch bhadi.
+            return;
+        }
+        console.log('functionComponent => mnin tbadal email');
+      },[email]) //mnin ytbadal email
+
+
+                     //---------------------------componentDidUpdate dyal password----------------
+            
+            useEffect(()=>{
+              if (password ==""){   // mnin tcharga awal mara mykhdamch bhadi.
+                   return;
+               }
+               console.log('functionComponent => mnin tbadal password');
+             },[password]) //mnin ytbadal email
+
+
      function handelEmail(e){   // must type function 
         setEmail(e.target.value)
     }
@@ -14,7 +43,7 @@ export  function LoginFormA() {
     <div>
        <h1>function example</h1> 
        <input value={email} onChange={handelEmail} placeholder='Email ...'/> <br></br>
-       <input type='password' value={password} onChange={handelPassword} placeholder='Password ...'/>
+       <input value={password} onChange={handelPassword} placeholder='Password ...'/>
 
 
     </div>
@@ -26,6 +55,25 @@ export class LoginFormB extends React.Component {
         email:'',
         password:''
     }
+        componentDidMount(){
+          console.log('classComponent => chargemet dyal page');
+        }
+        //email exemple
+        componentDidUpdate(prevProps,prevState){
+          if(prevState.email !== this.state.email){
+
+            console.log('classComponent => ila tbadlat email f component');
+          }
+          if(prevState.password !== this.password.email){
+
+            console.log('classComponent => ila tbadlat password f component');
+          }
+          console.log('classComponent => ila tbadlat chi haja f component');
+
+        }
+        componentWillUnmount(){
+        console.log('classComponent => mnin tatlgha l component');
+        }
    handelEmail=(e)=>{  // not type function or const cause we in class component
         this.setState({
                 email:e.target.value
@@ -41,7 +89,7 @@ export class LoginFormB extends React.Component {
       <div>
         <h1>Class example</h1>
         <input value={this.state.email} onChange={this.handelEmail} placeholder='Email ...'/> <br></br>
-        <input type='password' value={this.state.password} onChange={this.handelPassword} placeholder='password ...'/>
+        <input  value={this.state.password} onChange={this.handelPassword} placeholder='password ...'/>
       </div>
     )
   }
